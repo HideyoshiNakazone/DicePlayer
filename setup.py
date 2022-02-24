f4e892cf348eace4a14a0aee568bf1868aa1845e
Vitor Hideyoshi <vitor.h.n.batista@gmail.com>
2022-02-24 03:22:20 +0000

    Creation of Binary with PyInstaller and Removal of Cx_Freeze

--- setup.py
+++ setup.py
@@ -1,17 +1,9 @@
-import sys
-from cx_Freeze import setup, Executable
+import PyInstaller.__main__
 
-# Dependencies are automatically detected, but it might need fine tuning.
-# "packages": ["os"] is used as example only
-build_exe_options = {"packages": ["os","setproctitle","nomkl","numpy"],
-                     "bin_includes": ["/usr/bin/python3"],
-                     "excludes": ["tkinter"],
-                     "optimize": 0}
+name = 'diceplayer'
 
-name = "diceplayer"
-
-setup(
-    name = name,
-    version = "0.1",
-    executables = [Executable("diceplayer/__main__.py", target_name=name)]
-)
+PyInstaller.__main__.run([
+    'diceplayer/__main__.py',
+    '--onefile',
+    '-n{}'.format(name)
+])
